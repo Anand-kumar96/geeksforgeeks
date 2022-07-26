066946a954a28d8816076eaf011d4262343d07de
Anand kumar <106487247+aryananand01@users.noreply.github.com>
2022-07-26 14:23:01 +0530

    Update Reverse an Array.java

--- important/Reverse an Array.java
+++ important/Reverse an Array.java
@@ -27,3 +27,35 @@ class GFG {
 	}
 	}
 }
+
+
+// 2nd method
+import java.lang.*;
+import java.io.*;
+
+class GFG {
+	public static void main (String[] args) {
+	Scanner sc=new Scanner(System.in);
+	int tc=sc.nextInt();
+	while(tc-->0){
+	    int n=sc.nextInt();
+	    int arr[]=new int[n];
+	    for(int i=0;i<n;i++){
+	        arr[i]=sc.nextInt();
+	    }
+// 	    for(int i=0;i<n/2;i++){
+// 	        int temp=arr[i];
+// 	          arr[i]=arr[n-1-i];
+// 	          arr[n-1-i]=temp;
+// 	}
+
+// 	for(int i=0;i<n;i++){
+// 	System.out.print(arr[i]+" ");
+// 	}
+for(int i=n-1;i>=0;i--){
+	System.out.print(arr[i]+" ");
+	}
+	System.out.println();
+	}
+	}
+}
