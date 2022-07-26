b20271b38a61781371b5919160ef7b7e13fc5527
Anand kumar <106487247+aryananand01@users.noreply.github.com>
2022-07-26 14:24:49 +0530

    Create Sort The Array.java

---
+++ important/Sort The Array.java
@@ -0,0 +1,11 @@
+// https://practice.geeksforgeeks.org/problems/sort-the-array0055/1
+
+// code
+class Solution 
+{ 
+    int[] sortArr(int[] arr, int n) 
+    { 
+     Arrays.sort(arr);
+     return arr;
+    }
+} 
